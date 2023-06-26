14deae1200a0f2308505b2469f0d223282786871
mehul7169 <mehul7169@gmail.com>
2023-06-26 12:48:10 -0400

    Improved merge two sorted lists

--- MergeTwoSortedLists_Python.py
+++ MergeTwoSortedLists_Python.py
@@ -1,33 +1,15 @@
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        if list1 == None and list2 == None:
-            return ListNode().next
-        head = ListNode()
-        head.val = None
-        cur = head
-        while list1 != None or list2 != None:
-            nextNode = ListNode()
-            if list1 == None:
-                cur.val = list2.val
-                if list2.next == None:
-                    break
-                list2 = list2.next
-            elif list2 == None:
-                cur.val = list1.val
-                if list1.next == None:
-                    break
-                list1 = list1.next
-            elif list1.val < list2.val:
-                cur.val = list1.val
-                list1 = list1.next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        cur = dummy = ListNode()
+        while list1 and list2:               
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
             else:
-                cur.val = list2.val
-                list2 = list2.next
-            cur.next = nextNode
-            cur = nextNode
-        return head
+                cur.next = list2
+                list2, cur = list2.next, list2
+                
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            
+        return dummy.next
